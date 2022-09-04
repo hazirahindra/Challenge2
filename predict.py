882f6e976dde8745d537014a6732e7425c6b9b16
NURUL HAZIRAH <58581460+hazirahindra@users.noreply.github.com>
2022-09-05 01:36:51 +0800

    :bug: solve the print issue

--- predict.py
+++ predict.py
@@ -2,27 +2,25 @@ from tensorflow import keras
 
 from keras.models import load_model
 from keras.utils import load_img, img_to_array
-# from keras.preprocessing.image import img_to_array
 from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 from keras.applications.vgg16 import VGG16
 import numpy as np
+import tensorflow as tf
 
 import matplotlib.pyplot as plt
 import cv2
 
 model=load_model('model_saved.h5')
  
-image = load_img('dataset/test/royce.jpg', target_size=(224, 224))
+image = load_img('dataset/test/rsz_swift.png', target_size=(224, 224))
+
 img = np.array(image)
 img = img / 255.0
 img = img.reshape(1,224,224,3)
 label = model.predict(img)
-print("Predicted Class (0 - EV , 1- nonEV): ", label[0][0])
 
-if label<1:
-    print('The car is Electrical Vehicle')
-    
+if label<0.01:
+    print("This car is an Electric Car!")
 else:
-    print('The car is not Electrical Vehicle')
-   
+    print ("This car is not an Electric Car!")
